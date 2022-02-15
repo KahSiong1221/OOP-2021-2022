9b6a1ee3ff61f6a9a20475c2a01d0a464bca0f81
JX <user@Siong-MacBook-Pro.local>
2022-02-15 11:06:36 +0000

    sin cos

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -251,6 +251,14 @@ public class Loops extends PApplet
 				}
 				break;
 			}
+			case 9:
+			{
+				stroke(0);
+				background(255);
+				fill(0, 0, 255);
+				line(width / 2, height / 2, width / 2, 50);
+				
+			}
 		}
 	}
 }
