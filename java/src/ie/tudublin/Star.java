30031e187c55c597e8c177e3e5b2a502e8fece7d
JX <user@Siong-MacBook-Pro.local>
2022-03-28 12:57:57 +0100

    star

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -1,6 +1,7 @@
 package ie.tudublin;
 
 import processing.data.TableRow;
+import processing.core.PApplet;
 
 public class Star {
     private boolean hab;
@@ -10,6 +11,13 @@ public class Star {
     private float yG;
     private float zG;
     private float absMag;
+
+
+    @Override
+    public String toString() {
+        return "Star [absMag=" + absMag + ", displayName=" + displayName + ", distance=" + distance + ", hab=" + hab
+                + ", xG=" + xG + ", yG=" + yG + ", zG=" + zG + "]";
+    }
 
     public Star(TableRow tr)
     {
@@ -77,5 +85,22 @@ public class Star {
         this.absMag = absMag;
     }
 
-    
+    public void render(StarMap pa)
+    {
+        float x = PApplet.map(xG, -5, 5, pa.border, pa.width - pa.border);
+        float y = PApplet.map(yG, -5, 5, pa.border, pa.height - pa.border);
+        
+        pa.stroke(255, 255, 0);
+        pa.line(x, y - 5, x, y + 5);
+        pa.line(x - 5, y, x + 5, y);
+
+        pa.noFill();
+        pa.stroke(255, 0, 0);
+        pa.circle(x, y, absMag);
+
+        pa.fill(255);
+        pa.textSize(14);
+        pa.textAlign(PApplet.LEFT, PApplet.CENTER);
+        pa.text(displayName, x + 20, y);
+    }
 }
